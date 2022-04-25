80260f01e96c206be65c00b8ae2b2ef86d1f16ef
Gustavo França <gstvz.franca@gmail.com>
2022-04-25 12:54:36 -0300

    feat: implemented the assets files: AdidasLogo, ArrowIcon, Heart, NotificationIcon, Sneaker and Truck

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import AdidasLogo from "./AdidasLogo.svg";
+import ArrowIcon from "./ArrowIcon.svg";
+import Heart from "./Heart.svg";
+import NotificationIcon from "./NotificationIcon.svg";
+import Sneaker from "./Sneaker.svg";
+import Truck from "./Truck.svg";
+
+export const Arrow = ArrowIcon;
+export const DeliveryTruck = Truck;
+export const HeartIcon = Heart;
+export const Logo = AdidasLogo;
+export const Notification = NotificationIcon;
+export const SneakerImg = Sneaker;
